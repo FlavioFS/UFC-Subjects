1782a88574a91c8f39cb304619ccaa47c8ea5c47
FlavioFS <flaviofreitas.h@gmail.com>
2016-10-10 15:01:04 -0300

    (Improvement) Output generation now shows preview. | Clearer ordering for output files.

--- SO/Labs/Lab5/test-everything.sh
+++ SO/Labs/Lab5/test-everything.sh
@@ -1,59 +1,87 @@
 ## Attempts to generate output folder
+echo ""
+echo "Attempting to create output folder: out/"
 mkdir -p out/
 
+## User feedback
+echo "Generating outputs..."
+echo -en "Completion: 0%"
 
 
 ## Short processes only
 # Stats
-java -classpath class escalonador "../data/comparison/short.csv" 1 FCFS      > "out/short-stats-fcfs.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 1 SJF       > "out/short-stats-sjf.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 1 SJFP      > "out/short-stats-sjfp.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 1 RR 20     > "out/short-stats-rr.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 1 Priority  > "out/short-stats-priority.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 1 PriorityP > "out/short-stats-priorityp.txt"
+java -classpath class escalonador "data/comparison/short.csv" 1 FCFS      > "out/1.1.1. Sml stats FCFS.txt"
+java -classpath class escalonador "data/comparison/short.csv" 1 SJF       > "out/1.1.2. Sml stats SJF.txt"
+java -classpath class escalonador "data/comparison/short.csv" 1 SJFP      > "out/1.1.3. Sml stats SJFP.txt"
+java -classpath class escalonador "data/comparison/short.csv" 1 RR 20     > "out/1.1.4. Sml stats RR.txt"
+java -classpath class escalonador "data/comparison/short.csv" 1 Priority  > "out/1.1.5. Sml stats Priority.txt"
+java -classpath class escalonador "data/comparison/short.csv" 1 PriorityP > "out/1.1.6. Sml stats PriorityP.txt"
+
+## User feedback
+echo -en "\e[0K\rCompletion: 17%"
 
 # List
-java -classpath class escalonador "../data/comparison/short.csv" 2 FCFS      > "out/short-list-fcfs.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 2 SJF       > "out/short-list-sjf.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 2 SJFP      > "out/short-list-sjfp.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 2 RR 20     > "out/short-list-rr.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 2 Priority  > "out/short-list-priority.txt"
-java -classpath class escalonador "../data/comparison/short.csv" 2 PriorityP > "out/short-list-priorityp.txt"
+java -classpath class escalonador "data/comparison/short.csv" 2 FCFS      > "out/1.2.1. Sml list FCFS.txt"
+java -classpath class escalonador "data/comparison/short.csv" 2 SJF       > "out/1.2.2. Sml list SJF.txt"
+java -classpath class escalonador "data/comparison/short.csv" 2 SJFP      > "out/1.2.3. Sml list SJFP.txt"
+java -classpath class escalonador "data/comparison/short.csv" 2 RR 20     > "out/1.2.4. Sml list RR.txt"
+java -classpath class escalonador "data/comparison/short.csv" 2 Priority  > "out/1.2.5. Sml list Priority.txt"
+java -classpath class escalonador "data/comparison/short.csv" 2 PriorityP > "out/1.2.6. Sml list PriorityP.txt"
 
+
+## User feedback
+echo -en "\e[0K\rCompletion: 33%"
 
 
 ## Long processes only
 # Stats
-java -classpath class escalonador "../data/comparison/long.csv" 1 FCFS      > "out/long-stats-fcfs.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 1 SJF       > "out/long-stats-sjf.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 1 SJFP      > "out/long-stats-sjfp.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 1 RR 20     > "out/long-stats-rr.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 1 Priority  > "out/long-stats-priority.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 1 PriorityP > "out/long-stats-priorityp.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 FCFS      > "out/2.1.1. Lrg stats FCFS.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 SJF       > "out/2.1.2. Lrg stats SJF.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 SJFP      > "out/2.1.3. Lrg stats SJFP.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 RR 20     > "out/2.1.4. Lrg stats RR.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 Priority  > "out/2.1.5. Lrg stats Priority.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 PriorityP > "out/2.1.6. Lrg stats PriorityP.txt"
+
+## User feedback
+echo -en "\e[0K\rCompletion: 50%"
 
 # List
-java -classpath class escalonador "../data/comparison/long.csv" 2 FCFS      > "out/long-list-fcfs.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 2 SJF       > "out/long-list-sjf.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 2 SJFP      > "out/long-list-sjfp.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 2 RR 20     > "out/long-list-rr.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 2 Priority  > "out/long-list-priority.txt"
-java -classpath class escalonador "../data/comparison/long.csv" 2 PriorityP > "out/long-list-priorityp.txt"
+java -classpath class escalonador "data/comparison/long.csv" 2 FCFS      > "out/2.2.1. Lrg list FCFS.txt"
+java -classpath class escalonador "data/comparison/long.csv" 2 SJF       > "out/2.2.2. Lrg list SJF.txt"
+java -classpath class escalonador "data/comparison/long.csv" 2 SJFP      > "out/2.2.3. Lrg list SJFP.txt"
+java -classpath class escalonador "data/comparison/long.csv" 2 RR 20     > "out/2.2.4. Lrg list RR.txt"
+java -classpath class escalonador "data/comparison/long.csv" 2 Priority  > "out/2.2.5. Lrg list Priority.txt"
+java -classpath class escalonador "data/comparison/long.csv" 2 PriorityP > "out/2.2.6. Lrg list PriorityP.txt"
 
+
+## User feedback
+echo -en "\e[0K\rCompletion: 66%"
 
 
 ## Mixes processes
 # Stats
-java -classpath class escalonador "../data/comparison/mixed.csv" 1 FCFS      > "out/mixed-stats-fcfs.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 1 SJF       > "out/mixed-stats-sjf.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 1 SJFP      > "out/mixed-stats-sjfp.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 1 RR 20     > "out/mixed-stats-rr.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 1 Priority  > "out/mixed-stats-priority.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 1 PriorityP > "out/mixed-stats-priorityp.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 FCFS      > "out/3.1.1. Mix stats FCFS.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 SJF       > "out/3.1.2. Mix stats SJF.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 SJFP      > "out/3.1.3. Mix stats SJFP.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 RR 20     > "out/3.1.4. Mix stats RR.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 Priority  > "out/3.1.5. Mix stats Priority.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 PriorityP > "out/3.1.6. Mix stats PriorityP.txt"
+
+
+## User feedback
+echo -en "\e[0K\rCompletion: 83%"
+
 
 # List
-java -classpath class escalonador "../data/comparison/mixed.csv" 2 FCFS      > "out/mixed-list-fcfs.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 2 SJF       > "out/mixed-list-sjf.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 2 SJFP      > "out/mixed-list-sjfp.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 2 RR 20     > "out/mixed-list-rr.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 2 Priority  > "out/mixed-list-priority.txt"
-java -classpath class escalonador "../data/comparison/mixed.csv" 2 PriorityP > "out/mixed-list-priorityp.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 2 FCFS      > "out/3.2.1. Mix list FCFS.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 2 SJF       > "out/3.2.2. Mix list SJF.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 2 SJFP      > "out/3.2.3. Mix list SJFP.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 2 RR 20     > "out/3.2.4. Mix list RR.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 2 Priority  > "out/3.2.5. Mix list Priority.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 2 PriorityP > "out/3.2.6. Mix list PriorityP.txt"
+
+
+## User feedback
+echo -e "\e[0K\rCompletion: 100%"
+echo "Done! Please check the folder 'out/'"
+echo ""
