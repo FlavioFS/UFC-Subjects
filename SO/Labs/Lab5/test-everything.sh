ff9a0651c07d790064f90f34107e12ff69075c02
FlavioFS <flaviofreitas.h@gmail.com>
2016-10-10 15:30:13 -0300

    (Bugfix) 'test-everything.sh' was always overwriting the output.

--- SO/Labs/Lab5/test-everything.sh
+++ SO/Labs/Lab5/test-everything.sh
@@ -14,25 +14,25 @@ echo "Generating outputs..."
 echo -en "Completion: 0%"
 
 
-## Short processes only -----------------------------------------------------------------------------------------
+## Small processes only -----------------------------------------------------------------------------------------
 # Stats
-java -classpath class escalonador "data/comparison/short.csv" 1 FCFS      > "out/all the thingies/1.1.1. Sml stats FCFS.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 SJF       > "out/all the thingies/1.1.2. Sml stats SJF.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 SJFP      > "out/all the thingies/1.1.3. Sml stats SJFP.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 RR 20     > "out/all the thingies/1.1.4. Sml stats RR.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 Priority  > "out/all the thingies/1.1.5. Sml stats Priority.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 PriorityP > "out/all the thingies/1.1.6. Sml stats PriorityP.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 FCFS      > "out/all the thingies/1.1.1. Sml stats FCFS.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 SJF       > "out/all the thingies/1.1.2. Sml stats SJF.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 SJFP      > "out/all the thingies/1.1.3. Sml stats SJFP.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 RR 20     > "out/all the thingies/1.1.4. Sml stats RR.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 Priority  > "out/all the thingies/1.1.5. Sml stats Priority.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 PriorityP > "out/all the thingies/1.1.6. Sml stats PriorityP.txt"
 
 ## User feedback
 echo -en "\e[0K\rCompletion: 11%"
 
 # List
-java -classpath class escalonador "data/comparison/short.csv" 2 FCFS      > "out/all the thingies/1.2.1. Sml list FCFS.txt"
-java -classpath class escalonador "data/comparison/short.csv" 2 SJF       > "out/all the thingies/1.2.2. Sml list SJF.txt"
-java -classpath class escalonador "data/comparison/short.csv" 2 SJFP      > "out/all the thingies/1.2.3. Sml list SJFP.txt"
-java -classpath class escalonador "data/comparison/short.csv" 2 RR 20     > "out/all the thingies/1.2.4. Sml list RR.txt"
-java -classpath class escalonador "data/comparison/short.csv" 2 Priority  > "out/all the thingies/1.2.5. Sml list Priority.txt"
-java -classpath class escalonador "data/comparison/short.csv" 2 PriorityP > "out/all the thingies/1.2.6. Sml list PriorityP.txt"
+java -classpath class escalonador "data/comparison/small.csv" 2 FCFS      > "out/all the thingies/1.2.1. Sml list FCFS.txt"
+java -classpath class escalonador "data/comparison/small.csv" 2 SJF       > "out/all the thingies/1.2.2. Sml list SJF.txt"
+java -classpath class escalonador "data/comparison/small.csv" 2 SJFP      > "out/all the thingies/1.2.3. Sml list SJFP.txt"
+java -classpath class escalonador "data/comparison/small.csv" 2 RR 20     > "out/all the thingies/1.2.4. Sml list RR.txt"
+java -classpath class escalonador "data/comparison/small.csv" 2 Priority  > "out/all the thingies/1.2.5. Sml list Priority.txt"
+java -classpath class escalonador "data/comparison/small.csv" 2 PriorityP > "out/all the thingies/1.2.6. Sml list PriorityP.txt"
 
 
 ## User feedback
@@ -92,18 +92,18 @@ java -classpath class escalonador "data/comparison/mixed.csv" 2 PriorityP > "out
 echo -en "\e[0K\rCompletion: 66%"
 
 # Small
-java -classpath class escalonador "data/comparison/short.csv" 1 FCFS      >> "out/stats only/Sml stats.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 SJF       >> "out/stats only/Sml stats.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 SJFP      >> "out/stats only/Sml stats.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 RR 20     >> "out/stats only/Sml stats.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 Priority  >> "out/stats only/Sml stats.txt"
-java -classpath class escalonador "data/comparison/short.csv" 1 PriorityP >> "out/stats only/Sml stats.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 FCFS      >  "out/stats only/Sml stats.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 SJF       >> "out/stats only/Sml stats.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 SJFP      >> "out/stats only/Sml stats.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 RR 20     >> "out/stats only/Sml stats.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 Priority  >> "out/stats only/Sml stats.txt"
+java -classpath class escalonador "data/comparison/small.csv" 1 PriorityP >> "out/stats only/Sml stats.txt"
 
 ## User feedback
 echo -en "\e[0K\rCompletion: 77%"
 
 # Large
-java -classpath class escalonador "data/comparison/long.csv" 1 FCFS      >> "out/stats only/Lrg stats.txt"
+java -classpath class escalonador "data/comparison/long.csv" 1 FCFS      >  "out/stats only/Lrg stats.txt"
 java -classpath class escalonador "data/comparison/long.csv" 1 SJF       >> "out/stats only/Lrg stats.txt"
 java -classpath class escalonador "data/comparison/long.csv" 1 SJFP      >> "out/stats only/Lrg stats.txt"
 java -classpath class escalonador "data/comparison/long.csv" 1 RR 20     >> "out/stats only/Lrg stats.txt"
@@ -114,7 +114,7 @@ java -classpath class escalonador "data/comparison/long.csv" 1 PriorityP >> "out
 echo -en "\e[0K\rCompletion: 88%"
 
 # Mixed
-java -classpath class escalonador "data/comparison/mixed.csv" 1 FCFS      >> "out/stats only/Mix stats.txt"
+java -classpath class escalonador "data/comparison/mixed.csv" 1 FCFS      >  "out/stats only/Mix stats.txt"
 java -classpath class escalonador "data/comparison/mixed.csv" 1 SJF       >> "out/stats only/Mix stats.txt"
 java -classpath class escalonador "data/comparison/mixed.csv" 1 SJFP      >> "out/stats only/Mix stats.txt"
 java -classpath class escalonador "data/comparison/mixed.csv" 1 RR 20     >> "out/stats only/Mix stats.txt"
