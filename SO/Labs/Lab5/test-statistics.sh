18bac569592b943ebc5da5d2b30f536c1af7210f
FlavioFS <flaviofreitas.h@gmail.com>
2016-10-09 22:44:26 -0300

    (Edit) Separated tests: statistics, list, all.

---
+++ SO/Labs/Lab5/test-statistics.sh
@@ -0,0 +1,6 @@
+java -classpath class escalonador "data/fcfs.csv" 1 FCFS
+java -classpath class escalonador "data/sjf.csv" 1 SJF
+java -classpath class escalonador "data/sjfp.csv" 1 SJFP
+java -classpath class escalonador "data/rr.csv" 1 RR 20
+java -classpath class escalonador "data/priority.csv" 1 Priority
+java -classpath class escalonador "data/priorityp.csv" 1 PriorityP
