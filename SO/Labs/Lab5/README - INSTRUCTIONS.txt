4e80c169dcff4555cd0f50cbee147af587d855cd
FlavioFS <flaviofreitas.h@gmail.com>
2016-10-11 13:32:18 -0300

    (Feature) Graphs for ALL samples.

--- SO/Labs/Lab5/README - INSTRUCTIONS.txt
+++ SO/Labs/Lab5/README - INSTRUCTIONS.txt
@@ -5,10 +5,21 @@ RUN
 1. Run the makefile:
 	$ make
 
-2. Give permission to "test-everything.sh" to run as a program
+2. Give the following files permission to run as a program
 (Right click >> "Properties" >> "Permissions" >> "Execute" checkbox):
+	* test-everything.sh
+	* test-debug.sh
 
-3. Run it:
+3. The result of running them (in this order):
 	$ ./test-everything.sh
+		Outputs all the possible tests (small, large and mixed processes)
+		in the folder "out/".
 
-4. Outputs will be in folder "out/".
+	$ ./test-graphs.sh
+		Generates CSV files that serve as base to draw comparison
+		graphs in Octave/Matlab. Outputs to folder graphs/<file>.csv,
+		where the files may be 'small.csv', 'large.csv', 'mixed.csv'.
+
+4. Run gengraphs.m in octave to produce a comparison graph:
+	$ octave
+	$ gengraphs.mixed
--- SO/Labs/Lab5/graphs/gengraphs.m
+++ SO/Labs/Lab5/graphs/gengraphs.m
@@ -2,68 +2,103 @@ load small.csv
 load large.csv
 load mixed.csv
 
-%% Plot
-myfig = figure();
+disp ('Generating graphs...');
 
-% Labels
-labels = [
-	'FCFS',
-	'SJF',
-	'SJFP',
-	'RR',
-	'Priority',
-	'PriorityP'
-]
+% Values S
+throughputS = small(:, 3);
+turnaroundS = small(:, 4);
+waitingS    = small(:, 5);
+answerS     = small(:, 6);
+swapsS      = small(:, 7);
 
-% Values
-throughput = small(:, 3);
-turnaround = small(:, 4);
-waiting    = small(:, 5);
-answer     = small(:, 6);
-swaps      = small(:, 7);
+% Values L
+throughputL = large(:, 3);
+turnaroundL = large(:, 4);
+waitingL    = large(:, 5);
+answerL     = large(:, 6);
+swapsL      = large(:, 7);
 
-axes1 = axes('Parent', myfig, 'XTickLabel', labels);
-hold(axes1,'all');
+% Values M
+throughputM = mixed(:, 3);
+turnaroundM = mixed(:, 4);
+waitingM    = mixed(:, 5);
+answerM     = mixed(:, 6);
+swapsM      = mixed(:, 7);
 
-% Turnaround
-plot(turnaround, 'w-^',...
-     'LineWidth', 1,...
-     'MarkerSize', 7,...
-     'MarkerEdgeColor', 'w',...
-     'MarkerFaceColor', 'c'
-);
-hold on;
+%% gengraph: function description
+function [outputs] = gengraph(throughput, turnaround, waiting, answer, swaps)
+     % Labels
+     labels = [
+          'FCFS',
+          'SJF',
+          'SJFP',
+          'RR',
+          'Priority',
+          'PriorityP'
+     ];
 
-% Waiting Time
-plot(waiting, 'k-o',...
-     'LineWidth', 2,...
-     'MarkerSize', 10,...
-     'MarkerEdgeColor', 'k',...
-     'MarkerFaceColor', 'w'
-);
-hold on;
+     %% Plot
+     myfig = figure();
 
-% Answer Time
-plot(answer, 'g:d',...
-     'LineWidth', 2,...
-     'MarkerSize', 7,...
-     'MarkerEdgeColor', 'k',...
-     'MarkerFaceColor', 'g'
-);
-hold on;
+     max = 0;
+     for i = 1:6
+          if (max < throughput(i)) max = throughput(i); end;
+          if (max < turnaround(i)) max = turnaround(i); end;
+          if (max < waiting(i))    max = waiting(i);    end;
+          if (max < answer(i))     max = answer(i);     end;
+          if (max < swaps(i))      max = swaps(i);      end;
+     end
 
-% Swaps
-plot(swaps, 'm-v',...
-     'LineWidth', 2,...
-     'MarkerSize', 7,...
-     'MarkerEdgeColor', 'k',...
-     'MarkerFaceColor', 'm'
-);
-hold on;
+     axes1 = axes('Parent', myfig, 'XTickLabel', labels);
+     axis([1, 6, 0, max*1.3]);
+     hold(axes1,'all');
 
-daLegend = legend({'Turnaround', 'Waiting Time', 'Answer Time', 'Swaps'});
-set(daLegend,'color', 'none');
-set(daLegend,'FontSize', 10);
-set(daLegend,'FontWeight', 'bold');
-set(gca, 'color', [0.3 0.3 0.3]);  % Background color (chart area)
-set(gcf, 'color', [0.4 0.4 0.4]);  % Background color (area outside of chart)
+     % Turnaround
+     plot(turnaround, 'w-^',...
+          'LineWidth', 2,...
+          'MarkerSize', 7,...
+          'MarkerEdgeColor', 'k',...
+          'MarkerFaceColor', 'w'
+     );
+     hold on;
+
+     % Waiting Time
+     plot(waiting, 'r-s',...
+          'LineWidth', 2,...
+          'MarkerSize', 7,...
+          'MarkerEdgeColor', 'k',... 
+          'MarkerFaceColor', 'r'
+     );
+     hold on;
+
+     % Answer Time
+     plot(answer, 'g:o',...
+          'LineWidth', 2,...
+          'MarkerSize', 7,...
+          'MarkerEdgeColor', 'k',...
+          'MarkerFaceColor', 'g'
+     );
+     hold on;
+
+     % Swaps
+     plot(swaps, 'c-v',...
+          'LineWidth', 2,...
+          'MarkerSize', 7,...
+          'MarkerEdgeColor', 'k',...
+          'MarkerFaceColor', 'c'
+     );
+     hold on;
+
+     daLegend = legend({'Turnaround', 'Waiting Time', 'Answer Time', 'Swaps'});
+     set(daLegend,'color', 'none');
+     set(daLegend,'FontSize', 10);
+     set(daLegend,'FontWeight', 'bold');
+     set(gca, 'color', [0.3 0.3 0.3]);  % Background color (chart area)
+     set(gcf, 'color', [0.4 0.4 0.4]);  % Background color (area outside of chart)
+
+outputs = 0;
+end;
+
+gengraph(throughputS, turnaroundS, waitingS, answerS, swapsS);
+gengraph(throughputL, turnaroundL, waitingL, answerL, swapsL);
+gengraph(throughputM, turnaroundM, waitingM, answerM, swapsM);
