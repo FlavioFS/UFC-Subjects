dec237a459ff483c6e8763bf022c8f29e09352f1
FlavioFS <flaviofeitas.h@gmail.com>
2016-10-11 15:25:01 -0300

    (Tests) Precision improvement: More processes in test cases.

--- SO/Labs/Lab5/test-graphs.sh
+++ SO/Labs/Lab5/test-graphs.sh
@@ -1,9 +1,9 @@
 ## Attempts to generate output folder
 echo ""
 echo "Attempting to create output folder:"
-echo "    debug/"
+echo "    graphs/"
 echo ""
-mkdir -p "debug/"
+mkdir -p "graphs/"
 
 ## User feedback
 echo "Generating outputs..."
@@ -11,40 +11,40 @@ echo -en "Completion: 0%"
 
 ## Stats only ----------------------------------------------------------------------------------------------
 # Small
-echo "# <Processing>, <CPU>, <Throughput>, <Turn>, <Wait>, <Ans>, <Swaps>, <No. of Process>" > "debug/small.csv"
-java -classpath class escalonador "data/comparison/small.csv" 3 FCFS      >> "debug/small.csv"
-java -classpath class escalonador "data/comparison/small.csv" 3 SJF       >> "debug/small.csv"
-java -classpath class escalonador "data/comparison/small.csv" 3 SJFP      >> "debug/small.csv"
-java -classpath class escalonador "data/comparison/small.csv" 3 RR 20     >> "debug/small.csv"
-java -classpath class escalonador "data/comparison/small.csv" 3 Priority  >> "debug/small.csv"
-java -classpath class escalonador "data/comparison/small.csv" 3 PriorityP >> "debug/small.csv"
+echo "# <Processing>, <CPU>, <Throughput>, <Turn>, <Wait>, <Ans>, <Swaps>, <No. of Process>" > "graphs/small.csv"
+java -classpath class escalonador "data/comparison/small.csv" 3 FCFS      >> "graphs/small.csv"
+java -classpath class escalonador "data/comparison/small.csv" 3 SJF       >> "graphs/small.csv"
+java -classpath class escalonador "data/comparison/small.csv" 3 SJFP      >> "graphs/small.csv"
+java -classpath class escalonador "data/comparison/small.csv" 3 RR 20     >> "graphs/small.csv"
+java -classpath class escalonador "data/comparison/small.csv" 3 Priority  >> "graphs/small.csv"
+java -classpath class escalonador "data/comparison/small.csv" 3 PriorityP >> "graphs/small.csv"
 
 ## User feedback
 echo -en "\e[0K\rCompletion: 33%"
 
 # Large
-echo "# <Processing>, <CPU>, <Throughput>, <Turn>, <Wait>, <Ans>, <Swaps>, <No. of Process>" > "debug/large.csv"
-java -classpath class escalonador "data/comparison/long.csv" 3 FCFS      >> "debug/large.csv"
-java -classpath class escalonador "data/comparison/long.csv" 3 SJF       >> "debug/large.csv"
-java -classpath class escalonador "data/comparison/long.csv" 3 SJFP      >> "debug/large.csv"
-java -classpath class escalonador "data/comparison/long.csv" 3 RR 20     >> "debug/large.csv"
-java -classpath class escalonador "data/comparison/long.csv" 3 Priority  >> "debug/large.csv"
-java -classpath class escalonador "data/comparison/long.csv" 3 PriorityP >> "debug/large.csv"
+echo "# <Processing>, <CPU>, <Throughput>, <Turn>, <Wait>, <Ans>, <Swaps>, <No. of Process>" > "graphs/large.csv"
+java -classpath class escalonador "data/comparison/long.csv" 3 FCFS      >> "graphs/large.csv"
+java -classpath class escalonador "data/comparison/long.csv" 3 SJF       >> "graphs/large.csv"
+java -classpath class escalonador "data/comparison/long.csv" 3 SJFP      >> "graphs/large.csv"
+java -classpath class escalonador "data/comparison/long.csv" 3 RR 20     >> "graphs/large.csv"
+java -classpath class escalonador "data/comparison/long.csv" 3 Priority  >> "graphs/large.csv"
+java -classpath class escalonador "data/comparison/long.csv" 3 PriorityP >> "graphs/large.csv"
 
 ## User feedback
 echo -en "\e[0K\rCompletion: 66%"
 
 # Mixed
-echo "# <Processing>, <CPU>, <Throughput>, <Turn>, <Wait>, <Ans>, <Swaps>, <No. of Process>" > "debug/mixed.csv"
-java -classpath class escalonador "data/comparison/mixed.csv" 3 FCFS      >> "debug/mixed.csv"
-java -classpath class escalonador "data/comparison/mixed.csv" 3 SJF       >> "debug/mixed.csv"
-java -classpath class escalonador "data/comparison/mixed.csv" 3 SJFP      >> "debug/mixed.csv"
-java -classpath class escalonador "data/comparison/mixed.csv" 3 RR 20     >> "debug/mixed.csv"
-java -classpath class escalonador "data/comparison/mixed.csv" 3 Priority  >> "debug/mixed.csv"
-java -classpath class escalonador "data/comparison/mixed.csv" 3 PriorityP >> "debug/mixed.csv"
+echo "# <Processing>, <CPU>, <Throughput>, <Turn>, <Wait>, <Ans>, <Swaps>, <No. of Process>" > "graphs/mixed.csv"
+java -classpath class escalonador "data/comparison/mixed.csv" 3 FCFS      >> "graphs/mixed.csv"
+java -classpath class escalonador "data/comparison/mixed.csv" 3 SJF       >> "graphs/mixed.csv"
+java -classpath class escalonador "data/comparison/mixed.csv" 3 SJFP      >> "graphs/mixed.csv"
+java -classpath class escalonador "data/comparison/mixed.csv" 3 RR 20     >> "graphs/mixed.csv"
+java -classpath class escalonador "data/comparison/mixed.csv" 3 Priority  >> "graphs/mixed.csv"
+java -classpath class escalonador "data/comparison/mixed.csv" 3 PriorityP >> "graphs/mixed.csv"
 
 
 ## User feedback
 echo -e "\e[0K\rCompletion: 100%"
-echo "Done! Please check the folder 'debug/'"
+echo "Done! Please check the folder 'graphs/'"
 echo ""
